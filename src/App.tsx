7420e073ce3b9d639444d82a1054b8cfbcc5d7db
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-16 12:09:10 +0000

    Created functional component in separate file for nasa information to be displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nasa from "./nasa_page";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The code in <code>src/App.tsx</code> has been edited.
-        </p>
+        <Nasa title = "New title"/>
         <a
           className="App-link"
           href="https://reactjs.org"

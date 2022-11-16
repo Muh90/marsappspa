3d72e9f84fc5c3fce4e2a58d393a6450a4776ce6
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-16 12:39:36 +0000

    Included image in the info page template component.

--- src/nasa_page.tsx
+++ src/nasa_page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Info from "./info_page_template";
+import logo from './logo.svg';
 
 
 const Nasa = <Info
     title = "NASA"
     p1 = "This is a paragraph."
     p2 = "This is a second paragraph."
+    image_src = {logo}
+    image_className = "App-logo"
+    image_alt = "logo"
 />
 
 export default Nasa;
